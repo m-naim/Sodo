bb3f37c8621aa3f8bd013a17aa5d9d2f6f8c0e8b
m-naim <mehenaouin@gmail.com>
2019-01-30 16:58:20 +0100

    env support

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const config = require('./config/database.js');
-const routes= require('./app/routes.js')
+const routes= require('./app/routes.js');
 
+require('dotenv').config();
 mongoose.Promise = global.Promise;
 /*
 mongoose.connect(config.remoteUrl, { useNewUrlParser: true }).then(
@@ -23,13 +24,14 @@ app.use('/todo',routes);
 
 
   // ... other app.use middleware 
+  
   app.use(express.static(path.join(__dirname, "client", "build")))
   app.get("*", (req, res) => {
       res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
-
+  
 
 var port = process.env.PORT || 8080; 				// set the port
 app.listen(port, ()=>{
-    console.log('server runing on port n:'+port);
+    console.log('server runing on port n:'+port+process.env);
 });
