5d470059ebe6b733f0bf7c7b054bb9018bc52eef
m-naim <mehenaouin@gmail.com>
2019-02-05 22:05:55 +0100

    quotebox bug on deploying

--- client/src/scenes/dashbord/index.js
+++ client/src/scenes/dashbord/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import OptionsBar from '../../components/optionBar';
 import SideBar from '../../components/sideBar';
-import QuoteBox from '../../components/quoteBox';
+import QuoteBox from '../../components/quoteBox.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 
