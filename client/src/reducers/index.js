a69bfe54600cc92e0658f285cb2c9700d027164f
m-naim <mehenaouin@gmail.com>
2020-03-29 23:38:44 +0200

    client swap

--- client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import listReducer from './listReducer';
-import taskReducer from './taskReducer';
-
-export default combineReducers({
-  lists: listReducer,
-  tasks: taskReducer
-});
